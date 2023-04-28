49cc6323fb363347995aad126cdbc6c48845b004
liangjinyong <liangjy881@gmail.com>
2023-04-28 23:54:37 +0900

    ch02 java - first contact

--- app/src/main/java/com/westlife/westhunter/WestHunter.java
+++ app/src/main/java/com/westlife/westhunter/WestHunter.java
@@ -1,15 +1,91 @@
 package com.westlife.westhunter;
 
+// These are all the classes of other people's
+// (Android) code that we use for West Hunter
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.Window;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.view.Display;
+import android.widget.ImageView;
+import java.util.Random;
 
 public class WestHunter extends Activity {
 
+    /*
+    Android runs this code just before
+    the player sees the app.
+    This makes it a good place to add
+    the code for the one-time setup phase.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        Log.d("Debugging", "In onCreate");
+        newGame();
+        draw();
+    }
+
+    /*
+    This code will execute when a new
+    game needs to be started. It will
+    happen when the app is first started
+    and after the player wins a game.
+     */
+    void newGame() {
+        Log.d("Debugging", "In newGame");
+    }
+
+    /*
+    Here we will do all the drawing.
+    The grid lines, the HUD and
+    the touch indicator
+     */
+    void draw() {
+        Log.d("Debugging","In draw");
+    }
+
+    /*
+    This part of the code will
+    handle detecting that the player
+    has tapped the screen
+     */
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d("Debugging","In onTouchEvent");
+        takeShot();
+        return true;
+    }
+
+    /*
+    The code here will execute when
+    the player taps the screen. It will
+    calculate the distance from the sub'
+    and decide a hit or miss
+     */
+    void takeShot() {
+        Log.d("Debugging","In takeShot");
+        draw();
+    }
+
+    // This code says "BOOM!"
+    void boom() {
+
+    }
+
+    // This code prints the debugging text
+    void printDebuggingText() {
+
     }
 }
